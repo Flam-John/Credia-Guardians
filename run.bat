08cf386dd70c24a9185ddd7e5a77d4a97fdc95c8
Ioannis Flamouridis <john.flam11@gmail.com>
2026-07-07 20:40:50 +0300

    chore: add run.bat launcher

---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+rem Launch Credia Guardians (double-click me, or run from a terminal)
+"%LOCALAPPDATA%\Microsoft\WinGet\Packages\GodotEngine.GodotEngine_Microsoft.Winget.Source_8wekyb3d8bbwe\Godot_v4.7-stable_win64.exe" --path "%~dp0"
